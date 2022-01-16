67a9189b651cc546b41d8c2907ef74e7f980e7ff
Georgiy Bykov <zhora.bykov@gmail.com>
2022-01-17 00:40:35 +0300

    Add credo to deps with config - static code analysis tool

--- mix.exs
+++ mix.exs
@@ -50,8 +50,8 @@ defmodule Weather.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:httpoison, "~> 1.8"},
-
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:credo_contrib, "~> 0.2", only: [:dev, :test], runtime: false}
     ]
   end
 
